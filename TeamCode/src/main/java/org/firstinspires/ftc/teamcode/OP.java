f3930bb44f693477bbd92ebb41dceb7c241be2e7
BruceLi <liyifei723@gmail.com>
2021-05-09 21:55:04 +0800

    update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OP.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OP.java
@@ -1,27 +1,29 @@
 package org.firstinspires.ftc.teamcode;
 
-
 import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.telemetry.TelemetryPacket;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
 
+@TeleOp(name="OP", group="Linear Opmode")
 
-@TeleOp(name="test dashboard 3", group="Linear Opmode")
-
-public class TestDashboardTois extends LinearOpMode {
+public class OP extends LinearOpMode {
 
     private static final String VUFORIA_KEY =
             " Ac0FFOT/////AAABmXCneE7i4E7yh04pdQJzJLoJiMTtdAAblmjvED9fhNmHt4e0yFN4TffUBMeonbsrrhvdOrdOmUsxVQxe0hchzvvmeqSXKhR3Ozn6e9tiwJ7tJv2DCpPVgCPohRGMT/ZVdUi+IXnFA2tC1O+xfHJ/0Mtm6V4xq/WHNeP1IeIOTVewRYf9XtZBhkpS+Fa+SdCsnalhvv1sF2Ss+wz9m6cUKo6KoEz80ItsN2UUTlxMsnOQaAXfELJ/zeTCk7Nl4u8T5E+aoeLjI31HXUSYzy/JsT9dUUXCPDz95KHLWm2nxRPlciqLik6ZLJKCMYzdn37t8CjYkZxKmrvCFMgjrkErM4a/UtCLqM3iyr8Sb/fnSOdW ";
 
     WebcamName webcamName = null;
 
-    // Declare OpMode members.
+    // Declare OpMode members.wo
     DcMotorEx x, y, y2;
+
+    DcMotorEx LF, LB, RF, RB, intake, shooter;
 
     // encoder position tracking parameters
 
@@ -31,6 +33,8 @@ public class TestDashboardTois extends LinearOpMode {
     final double DISTANCE_BETWEEN_AXIS = 415;
     final double CIRCUM = DISTANCE_BETWEEN_AXIS * Math.PI;
     final double SIDE_LENGTH = 10;
+
+    double xVal, yVal, triggerVal;
 
 
     @Override
@@ -43,6 +47,21 @@ public class TestDashboardTois extends LinearOpMode {
         double xDistance, yTurn, rotationAngle, yTurn2;
 
         webcamName = hardwareMap.get(WebcamName.class, "Webcam 1");
+
+        LF = hardwareMap.get(DcMotorEx.class,"LF");
+        LB = hardwareMap.get(DcMotorEx.class,"LB");
+        RF = hardwareMap.get(DcMotorEx.class,"RF");
+        RB = hardwareMap.get(DcMotorEx.class,"RB");
+
+        intake = hardwareMap.get(DcMotorEx.class,"x");
+        shooter = hardwareMap.get(DcMotorEx.class,"y");
+//        intake = hardwareMap.get(DcMotorEx.class,"intake");
+//        shooter = hardwareMap.get(DcMotorEx.class, "shooter");
+
+        LF.setDirection(DcMotorEx.Direction.FORWARD);
+        LB.setDirection(DcMotorEx.Direction.FORWARD);
+        RF.setDirection(DcMotorEx.Direction.REVERSE);
+        RB.setDirection(DcMotorEx.Direction.REVERSE);
 
         // Wait for the game to start (driver presses PLAY)
         waitForStart();
@@ -67,6 +86,34 @@ public class TestDashboardTois extends LinearOpMode {
 
             TelemetryPacket packet = new TelemetryPacket();
 
+            yVal =  -1 * gamepad1.left_stick_y;
+            xVal =  1 * gamepad1.right_stick_x;
+            triggerVal = 0.75 * (gamepad1.right_trigger - gamepad1.left_trigger);
+
+            double lfPower = Range.clip(yVal + xVal + triggerVal, -1.0, 1.0) ;
+            double lbPower = Range.clip(yVal - xVal + triggerVal, -1.0, 1.0) ;
+            double rfPower = Range.clip(yVal - xVal - triggerVal, -1.0, 1.0) ;
+            double rbPower = Range.clip(yVal + xVal - triggerVal, -1.0, 1.0) ;
+
+            // Send calculated power to wheels
+            LF.setPower(lfPower);
+            LB.setPower(lbPower);
+            RF.setPower(rfPower);
+            RB.setPower(rbPower);
+
+
+            if (gamepad1.b) {
+                intake.setPower(0.75);
+            } else if (gamepad1.a) {
+                intake.setPower(-0.75);
+            }
+
+            if(gamepad1.right_bumper){
+                shooter.setPower(1);
+            }
+            else{
+                shooter.setPower(0);
+            }
             xDistance = (double) x.getCurrentPosition() / TICKS_PER_REV * DISTANCE_PER_REV;
             yTurn = (double) y.getCurrentPosition() / TICKS_PER_REV * DISTANCE_PER_REV;
             yTurn2 = -(double) y2.getCurrentPosition() / TICKS_PER_REV * DISTANCE_PER_REV;
